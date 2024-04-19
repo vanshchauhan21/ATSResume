3b85b8d61d720c57059681641e0b2a680be2823c
vanshchauhan21 <143607775+vanshchauhan21@users.noreply.github.com>
2024-04-19 17:22:18 +0530

    first commit

---
+++ components/utility/DefaultResumeData.jsx
@@ -0,0 +1,88 @@
+const DefaultResumeData = {
+  name: "VANSH CHAUHAN",
+  position: "Software Developer",
+  contactInformation: "6397976104",
+  email: "chauhanvansh313@gmail.com",
+  address: "Bijnor ,Uttarpradesh ",
+  profilePicture: "",
+  socialMedia: [
+    {
+      socialMedia: "Github",
+      link: "github.com//vanshchauhan21",
+    },
+    {
+      socialMedia: "LinkedIn",
+      link: "linkedin.com/in/vanshchauhan21",
+    },
+    {
+      socialMedia: "Website",
+      link: "ozippa.com",
+    },
+  ],
+  summary: "Resourceful Developer with 11 years of experience in designing and developing user interfaces, testing and training employees. Skilled at utilizing a wide variety of tools and programs to provide effective applications.",
+  education: [
+    {
+      "school": "Bachelor of Computer Science",
+      "degree": "Dr. A.P.J. Abdul Kalam Technical University",
+      "startYear": "2021-08-20",
+      "endYear": "2025-07-01"
+    },
+  ],
+  workExperience: [
+    {
+      "company": "Torph TTC",
+      "position": "Developer",
+      "description": "Torph TTC is a global software company that offers user interface UI development tools and components for a range of developer applications across all platforms.",
+      "keyAchievements": "Created and maintained 10 web applications for numerous national and foreign clients.\nEnsured that the user interfaces and user experience of the software applications developed by the team met at least 80% of users expectations.\nCreated and analyzed 500 unit test cases.\nDeveloped python scripts to automate image's noise-reduction process which helped improve research analysis time by 40%.\nEstablished and lead a team of 10 people; covering every key role in the early stages.",
+      "startYear": "2023-02-16",
+      "endYear": "2023-02-21"
+    },
+    {
+      "company": "Reilty Group",
+      "position": "Front End Web Developer",
+      "description": "Reilty Group is an industry-leading provider of online gambling software and solutions.",
+      "keyAchievements": "Increased by 35% the reach of users to the platform, over the installation of the web platform in mobile devices.\nDelivered 30 web solutions.\nReplaced 1 full-time web developer under part-time employment achieving a productivity gain of over 60% as measured by working hours.\nWorked with more than 4 different teams at different points of time and provided end-to-end solutions for clients.",
+      "startYear": "2023-02-22",
+      "endYear": "2023-03-09"
+    },
+    {
+      "company": "HellX",
+      "position": "Python Developer",
+      "description": "Heller is a Software Editor specialized in logistics management.",
+      "keyAchievements": "Developed several automation systems - saved over $50,000 annually.\nJoined the team as #10 employee as a developer working directly with the biggest customers.\nInvolved in coding as per the requirement, delivered well ahead of time which saved project cost by 20%.\nReduced database size by nearly 50% and improved its performance by 20%.\nDelivery time of good reduced by 50%.",
+      "startYear": "2023-02-22",
+      "endYear": "2023-03-09"
+    }
+  ],
+  projects: [],
+  skills: [
+    {
+      title: "Technical Skills",
+      skills: [
+        "Js", "Python", "Web Services", "C++", "HTML", "CSS", "SQL", "User Interface", "Creativity"
+      ]
+    },
+    {
+      title: "Soft Skills",
+      skills: [
+        "Collaboration", "Problem-solving", "Communication", "Time management", "Result-oriented"
+      ]
+    },
+    {
+      title: "Additional Skills",
+      skills: [
+        "Public Speaking", "Writing", "Research"
+      ] 
+    }
+  ],
+  languages: [
+    "English",
+    "Hindi",
+  ],
+  certifications: [
+    "Certified Web Professional-Web Developer",
+    "Java Development Certified Professional",
+  ],
+};
+
+export default DefaultResumeData;
